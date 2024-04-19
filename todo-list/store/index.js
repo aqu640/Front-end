aed28025118435eaafecc9a2ee0f7f7d4781bf82
RITA <139612549+aqu640@users.noreply.github.com>
2024-04-19 11:28:00 +0800

    Update index.js

--- todo-list/store/index.js
+++ todo-list/store/index.js
@@ -1,14 +1,16 @@
 export const state = () => ({
 	tasks: []
 })
-
+	//add
 export const mutations = {
 	ADD_TASK(state, task) {
 		state.tasks = [{ content: task, done: false }, ...state.tasks];
 	},
+	//delete
 	REMOVE_TASK(state, task) {
 		state.tasks.splice(state.tasks.indexOf(task), 1);
 	},
+	//change
 	TOGGLE_TASK(state, task) {
 		task.done = !task.done;
 	}
